bf49dc28f15398a20dd6d42a8e00b7c2c6727a46
Michael Kolbeck <michael.b.kolbeck@gmail.com>
2021-11-01 16:16:59 -0400

    root endpoint returns synthea data as zip if synthea is adjacent

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Response
+import uuid
+import os
+import zipfile
+import io
+import shutil
+from starlette.responses import StreamingResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    directory_uuid = str(uuid.uuid4())
+    synthea_dir = "../synthea/" + directory_uuid
+    os.mkdir(synthea_dir)
+    os.system("cd ../synthea && ./run_synthea --exporter.baseDirectory  \"" + directory_uuid + "\"")
+    shutil.make_archive(synthea_dir, 'zip', synthea_dir)
+
+    s = open(synthea_dir + ".zip", "rb")
+
+    resp = StreamingResponse(s, media_type="application/x-zip-compressed", headers={
+        'Content-Disposition': f'attachment;filename={directory_uuid+".zip"}'
+    })
+
+    os.system("rm -rf " + synthea_dir)
+    os.system("rm " + synthea_dir + ".zip")
+
+    return resp
+
+    # return {"message": directory_uuid}
